e508582625c9f7303918461948242ddcdc757229
Adar <theadarbutel@gmail.com>
2017-01-28 14:57:34 -0800

    Added Redirect and Honeypot to Form

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var handleModal = function() {
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-    
+
     // When the user clicks on the button, open the modal
     btn.onclick = function() {
         modal.style.display = "block";
