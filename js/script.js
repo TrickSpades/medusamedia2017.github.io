1e91c67622cd11614f5ae10f6679c39daa271a08
Adar <theadarbutel@gmail.com>
2017-01-27 19:33:47 -0800

    Removed prints

--- js/script.js
+++ js/script.js
@@ -7,11 +7,7 @@ var handleModal = function() {
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-
-    console.log(modal);
-    console.log(btn);
-    console.log(span);
-
+    
     // When the user clicks on the button, open the modal
     btn.onclick = function() {
         modal.style.display = "block";
