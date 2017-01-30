5f78c68d90273d69efdeafc4d1d371271ce131ab
Adar <theadarbutel@gmail.com>
2017-01-29 23:01:50 -0800

    Switched to animatedModal.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-var handleModal = function() {
-    // Get the modal
-    var modal = document.getElementById('myModal');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("cta");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}
-
 var showPage = function(page) {
     var web = document.getElementById("web");
     var photos = document.getElementById("photos");
@@ -49,8 +21,6 @@ var showPage = function(page) {
             $("#videos-btn").addClass("current");
             break;
     }
-
-    console.log(page);
 }
 
 var hidePages = function() {
@@ -85,6 +55,5 @@ var setupButtons = function() {
 }
 
 $(document).ready(function() {
-    handleModal();
     setupButtons();
 })
