8dbf1502364c10443b9fe427ca022547824820ac
Adar <theadarbutel@gmail.com>
2017-02-03 21:47:25 -0800

    Redid Entire Site like a Boss

---
+++ js/home.js
@@ -0,0 +1,14 @@
+// Page Scrolling
+$(document).on('click', '.page-link', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+});
+
+$(document).ready(function() {
+    $("#nav-modal-text a").click(function() {
+        $(".close-nav-modal").click();
+    })
+})
