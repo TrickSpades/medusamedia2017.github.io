b9989383c129f6d667513f53504627e54e4b5436
Adar <theadarbutel@gmail.com>
2017-02-09 13:56:24 -0800

    Added Lazy Loading to Index and Photos

--- js/home.js
+++ js/home.js
@@ -11,4 +11,9 @@ $(document).ready(function() {
     $("#nav-modal-text a").click(function() {
         $(".close-nav-modal").click();
     })
-})
+
+    $(".lazy").viewportChecker({
+        classToAdd: "visible",
+        classToRemove: "lazy"
+    });
+});
